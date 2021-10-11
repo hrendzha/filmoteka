f1e70be84b261a34a3fd8f40654fd2f20e9a3beb
Pavlo Hrendzha <pashahrendzha99@gmail.com>
2021-10-11 23:39:10 +0300

    part of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './sass/main.scss';
 import filmsCards from './templates/films.hbs';
+
 const TMDB_API_KEY = 'e51fa7aa1819bb081f9c2dbbae1f5e9d';
+
+import addFilmInWatched from './js/add-films-in-lc';
+
+const div = document.querySelector('div');
+
+div.addEventListener('click', e => {
+  if (e.target.nodeName === 'BUTTON') {
+    const { target: btn } = e;
+
+    const id = Math.floor(Math.random() * 100);
+
+    addFilmInWatched(id);
+    console.log(btn.dataset.action);
+  }
+});
---
+++ src/js/local-storage.js
@@ -0,0 +1,20 @@
+const load = key => {
+  try {
+    const data = localStorage.getItem(key);
+
+    return data ? JSON.parse(data) : undefined;
+  } catch (err) {
+    console.error('Get state error: ', err);
+  }
+};
+
+const save = (key, value) => {
+  try {
+    const data = JSON.stringify(value);
+    localStorage.setItem(key, data);
+  } catch (err) {
+    console.error('Set state error: ', err);
+  }
+};
+
+export default { load, save };
