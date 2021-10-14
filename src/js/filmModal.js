1a9630ee25a82418e832b3ba6d926a30a87dd489
OlesiaNazarenko <eightpa8@gmail.com>
2021-10-14 12:26:04 +0300

    working modal window

--- src/js/filmModal.js
+++ src/js/filmModal.js
@@ -1,5 +1,7 @@
 import * as basicLightbox from 'basiclightbox'
 import '../../node_modules/basiclightbox/dist/basicLightbox.min.css'
+import modalTmp from '../templates/filmModal'
+import FilmsAPI from '../js/fetchFilmClass'
 
 const filmModalOpen = document.querySelector('.list-movies')
 const modalClose = document.querySelector('.modal_close');
@@ -8,13 +10,23 @@ const overlay = document.querySelector('.overlay');
 const instance = basicLightbox.create(overlay)
 
 
-    
+function renderModalContent(film) {
+    const modalMarkup = modalTmp(film)
+    modalClose.insertAdjacentHTML('afterend', modalMarkup);
+}
+
+function createModal(e) {
+    // const id = e.target.dataset.id;
+    FilmsAPI.getFilmById(438631).then((r) => { renderModalContent(r) })
+
+}
+
 filmModalOpen.addEventListener('click', (e) => {
     if (e.target.classList.contains('movie__img')) {
         instance.show()
+        createModal()
         document.addEventListener('keydown', closeModal);
         modalClose.addEventListener('click', closeModal);
-        console.log('ghghjg')
     }
 })
 
@@ -23,7 +35,5 @@ function closeModal(e) {
         instance.close()
         document.removeEventListener('keydown', closeModal);
         modalClose.removeEventListener('click', closeModal);
-        // overlay.innerHTML = '';
-        console.log('gghhg')
     }
     }
