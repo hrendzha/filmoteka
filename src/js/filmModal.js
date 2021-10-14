8a87317cee4fc8d008f98ae6ccb9c885e43b4c17
OlesiaNazarenko <eightpa8@gmail.com>
2021-10-14 14:06:17 +0300

    modal done

--- src/js/filmModal.js
+++ src/js/filmModal.js
@@ -6,25 +6,23 @@ import FilmsAPI from '../js/fetchFilmClass'
 const filmModalOpen = document.querySelector('.list-movies')
 const modalClose = document.querySelector('.modal_close');
 const overlay = document.querySelector('.overlay');
-
+const modalContent = document.querySelector('.modal_content')
 const instance = basicLightbox.create(overlay)
-
 
 function renderModalContent(film) {
     const modalMarkup = modalTmp(film)
-    modalClose.insertAdjacentHTML('afterend', modalMarkup);
+    modalContent.insertAdjacentHTML('beforeend', modalMarkup);
 }
 
-function createModal(e) {
-    // const id = e.target.dataset.id;
-    FilmsAPI.getFilmById(438631).then((r) => { renderModalContent(r) })
-
+function createModal(id) {
+    FilmsAPI.getFilmById(id).then((result) => { renderModalContent(result) })
 }
 
 filmModalOpen.addEventListener('click', (e) => {
     if (e.target.classList.contains('movie__img')) {
+        console.log(e.target.parentNode.parentNode.dataset.id)
         instance.show()
-        createModal()
+        createModal(e.target.parentNode.parentNode.dataset.id)
         document.addEventListener('keydown', closeModal);
         modalClose.addEventListener('click', closeModal);
     }
@@ -33,6 +31,7 @@ filmModalOpen.addEventListener('click', (e) => {
 function closeModal(e) {
     if (e.key === 'Escape' || e.target.classList.contains('icon-modal-close')){
         instance.close()
+        console.log(modalContent)
         document.removeEventListener('keydown', closeModal);
         modalClose.removeEventListener('click', closeModal);
     }
