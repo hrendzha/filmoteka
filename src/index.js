202174e599989efbaa2157f1b3e60e5a299df494
Vladyslav Starodubets <dp181191svn@gmail.com>
2021-10-17 12:48:11 +0200

    create team modal

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import filmsCards from './templates/films.hbs';
 import RenderFilmsClas from './js/renderFilmsClass';
 const TMDB_API_KEY = 'e51fa7aa1819bb081f9c2dbbae1f5e9d';
 RenderFilmsClas.renderTrendingMovies();
+
+import './js/footer-modal.js'
